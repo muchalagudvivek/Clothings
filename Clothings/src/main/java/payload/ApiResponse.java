af7e06a164cad27512c75c623978e4fad6aa2c3f
Shubham9260 <Shubham926093@gmail.com>
2024-03-20 23:00:26 +0530

    payload

---
+++ Clothings/src/main/java/payload/ApiResponse.java
@@ -0,0 +1,15 @@
+package payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+
+    private String Message;
+
+
+}
---
+++ Clothings/src/main/java/payload/PaymentDetails.java
@@ -0,0 +1,17 @@
+package payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+
+public class PaymentDetails {
+    private String orderId;
+    private Integer amount;
+    private String currency;
+
+    private String key;
+}
