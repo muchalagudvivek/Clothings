06c0b8037e88adfbac71f0df87989ff0cece25ea
Vivek Muchalagudd <muchalagudvivek484@gmail.com>
2024-03-21 14:48:50 +0530

    fixes

--- ClothingBackend/src/main/java/com/clothing/config/SecurityConfiguration.java
+++ ClothingBackend/src/main/java/com/clothing/config/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static com.organica.entities.TotalRoles.ADMIN;
+import static com.clothing.entities.TotalRoles.ADMIN;
 
 @Configuration
 @EnableWebSecurity
