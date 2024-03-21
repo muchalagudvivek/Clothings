48fb81c430d662a72c19f48931a94507b9938a6b
Bhavani-gudumala <bhavanigudumala@gmail.com>
2024-03-21 13:49:22 +0530

    changes

--- ClothingBackend/src/main/java/com/clothing/repo/UserRepo.java
+++ ClothingBackend/src/main/java/com/clothing/repo/UserRepo.java
@@ -4,5 +4,6 @@ import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-    public User findByEmail(String e);
+    public User findByEmail(User username); 
 }
+ 
