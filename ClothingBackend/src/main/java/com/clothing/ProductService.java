bc00fdc9d88e38785f5c37f0836f5f1db541caaa
Naveen Reddy <141206651+naveenreddy2711@users.noreply.github.com>
2024-03-21 16:04:55 +0530

    Add files via upload

---
+++ ClothingBackend/src/main/java/com/clothing/ProductService.java
@@ -0,0 +1,30 @@
+
+package com.clothing.services;
+
+import com.clothing.Payload.ProductDto;
+
+import java.util.List;
+
+public interface ProductService {
+
+    //create
+    ProductDto CreateProduct(ProductDto productDto);
+
+    //read
+    ProductDto ReadProduct(Integer ProductId);
+
+
+    //readAll
+    List<ProductDto> ReadAllProduct();
+
+
+    //delete
+    void DeleteProduct(Integer productId);
+
+
+    //update
+    ProductDto UpdateProduct(ProductDto productDto,Integer ProductId);
+
+
+
+}
